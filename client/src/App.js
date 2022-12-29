e88621639370ffad67e4291ca7f55c673952adad
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-12-29 21:24:17 +0530

    added google analytics to track pageviews

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,11 @@ import io from "socket.io-client"
 import { Physics } from '@react-three/cannon';
 import Hurdle from './components/Hurdle';
 import { useEnvironment } from '@react-three/drei';
+import ReactGA from "react-ga"
 // const socket = io.connect("http://localhost:3001")
 
+ReactGA.initialize('G-XQ7TKF8X99')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 function App() {
 
