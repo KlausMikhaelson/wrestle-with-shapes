c2a1d1fa684764f7f0520358047452e19f6a8627
Bhaskar Agrawal <8dbdown@gmail.com>
2023-02-24 19:55:22 +0530

    firstCommit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Environment, OrbitControls } from "@react-three/drei"
 import Ground from './components/ground';
 import Player from './components/Player';
 import './App.css';
-import io from "socket.io-client"
+// import io from "socket.io-client"
 import { Physics } from '@react-three/cannon';
 import Hurdle from './components/Hurdle';
 import { useEnvironment } from '@react-three/drei';
